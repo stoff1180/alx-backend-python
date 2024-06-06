bf13c55a89e0a3d4a82a26fa93658433145e3932
stoff1180 <stoffmustapha1180@gmail.com>
2024-06-06 08:38:59 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Function add that takes two float a and b as arguments
+    Returns their sum as a float
+'''
+
+
+def add(a: float, b: float) -> float:
+    ''' Return sum of a and b '''
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Function that takes two strings str1 and str2 as arguments
+    and returns a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' Returns concatenation of str1 and str2. '''
+    return str1 + str2
